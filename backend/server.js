5f6e351e13f2a7e92d13528fa0c9bba124d6a82e
Lucy Joshua <lucyalfred769@gmail.com>
2024-10-11 17:15:39 +0100

    First commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = 7241;
 
 // Enable CORS to allow requests from different origins (like React frontend)
 app.use(cors());
